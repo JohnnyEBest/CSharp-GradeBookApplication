0d7d31529fe3043eac3ba2bcecbb554d19690e55
JohnnyEBest <johnnybeezee@gmail.com>
2020-10-29 16:32:02 -0700

    Completed module 4 take 2

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -52,7 +52,7 @@ namespace GradeBook.UserInterfaces
             }
             else
             {
-                Console.WriteLine($"{0} is not a supported type of gradebook, please try again", TypeOfGradeBook);
+                Console.WriteLine($"{parts[2]} is not a supported type of gradebook, please try again");
                 return;
             }
             Console.WriteLine("Created gradebook {0}.", name);
