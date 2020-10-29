a56419e28023715a6e4c7af8007082184f01579f
JohnnyEBest <johnnybeezee@gmail.com>
2020-10-29 15:22:58 -0700

    Completed module Take2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,6 +18,17 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("You must have atleast 5 students to rank by grades.");
             }
+            var dropLetterGrade = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+            if (grades[dropLetterGrade-1] <= averageGrade)
+                return 'A';
+            else if (grades[(dropLetterGrade * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(dropLetterGrade * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(dropLetterGrade * 4) - 1] <= averageGrade)
+                return 'D';
+            else
                 return 'F';
         }
     }
