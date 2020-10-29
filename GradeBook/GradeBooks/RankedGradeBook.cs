d98deec0879c815f04eac7ab6f4436bac39fc189
JohnnyEBest <johnnybeezee@gmail.com>
2020-10-29 14:50:51 -0700

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -18,17 +18,6 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("You must have atleast 5 students to rank by grades.");
             }
-
-            if (averageGrade >= 90)
-                return 'A';
-            else if (averageGrade >= 80)
-                return 'B';
-            else if (averageGrade >= 70)
-                return 'C';
-            else if (averageGrade >= 60)
-                return 'D';
-            else
-                return 'F';
         }
     }
 
